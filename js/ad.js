183346863b594ac3ea220587328a08685596ce5e
Ujjwal Raj <ujjwalraj@Ujjwals-MacBook-Air.local>
2026-03-21 20:10:01 +0530

    ad section added

---
+++ js/ad.js
@@ -0,0 +1,8 @@
+const ads = [
+  {
+    title: "Just One More Page",
+    description: "Join the club to encourage reading! We pick books across science, technology, AI, philosophy, physics, and more.",
+    image: "images/ad/justOneMorePage.png",
+    url: ""
+  }
+];
