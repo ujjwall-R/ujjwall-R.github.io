b3784fd548794a5f768c8af13a81eb638aa9288d
Ujjwal Raj <ujjwalraj@Ujjwals-MacBook-Air.local>
2026-03-21 20:25:43 +0530

    ad correction

--- js/ad.js
+++ js/ad.js
@@ -1,7 +1,7 @@
 const ads = [
   {
     title: "Just One More Page",
-    description: "Join the club to encourage reading! We pick books across science, technology, AI, philosophy, physics, and more.",
+    description: "Join the club to encourage reading! We pick books across physics, technology, AI, philosophy, mathematics, and more.",
     image: "images/ad/justOneMorePage.png",
     url: ""
   }
